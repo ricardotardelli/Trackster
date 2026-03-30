48cb557a0d6dca6c9d5abfe66627d2d83be5b7a0
Ricardo Tardelli <ricardotardelli@outlook.com>
2026-03-30 01:48:41 +0100

    added authentication using Cognito

--- generator/frontend/src/app/auth/cognito.config.ts
+++ generator/frontend/src/app/auth/cognito.config.ts
@@ -1,7 +1,7 @@
 export const cognitoConfig = {
   userPoolId: 'us-east-1_rZmUAolzZ',
   userPoolClientId: '7g4slp3sne6rsvtpiacglgjt8o',
-  domain: 'https://us-east-1rzmuaolzz.auth.us-east-1.amazoncognito.com/login?client_id=7g4slp3sne6rsvtpiacglgjt8o&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwww.trackster.pt%2F',
+  domain: 'us-east-1-rzmuaolzz.auth.us-east-1.amazoncognito.com',
   redirectSignIn: 'https://www.trackster.pt/',
   redirectSignOut: 'https://www.trackster.pt/',
   scopes: ['openid', 'email']
