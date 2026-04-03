6404a5633d9af21a5c810810e1823860500d405f
Ricardo Tardelli <ricardotardelli@outlook.com>
2026-04-03 17:31:19 +0100

    fixed render before auth

--- generator/frontend/src/app/auth/cognito.config.ts
+++ generator/frontend/src/app/auth/cognito.config.ts
@@ -1,6 +1,6 @@
 export const cognitoConfig = {
   userPoolId: 'us-east-1_rZmUAolzZ',
-  userPoolClientId: '7g4slp3sne6rsvtpiacglgjt8o',
+  userPoolClientId: '2uf2smjrrjtt2rrr7r6bukv7s3',
   domain: 'us-east-1rzmuaolzz.auth.us-east-1.amazoncognito.com',
   redirectSignIn: 'https://www.trackster.pt/',
   redirectSignOut: 'https://www.trackster.pt/',
