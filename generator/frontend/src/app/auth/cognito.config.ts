c7d806595f972b0a656294e26e09c9107d959580
Ricardo Tardelli <ricardotardelli@outlook.com>
2026-04-08 22:35:14 +0100

    changed domain to studio.trackster.pt

--- generator/frontend/src/app/auth/cognito.config.ts
+++ generator/frontend/src/app/auth/cognito.config.ts
@@ -2,8 +2,8 @@ export const cognitoConfig = {
   userPoolId: 'us-east-1_rZmUAolzZ',
   userPoolClientId: '2uf2smjrrjtt2rrr7r6bukv7s3',
   domain: 'us-east-1rzmuaolzz.auth.us-east-1.amazoncognito.com',
-  redirectSignIn: 'https://studio.trackster.pt/',
-  redirectSignOut: 'https://studio.trackster.pt/',
+  redirectSignIn: 'https://studio.trackster.pt',
+  redirectSignOut: 'https://studio.trackster.pt',
   scopes: ['openid', 'email']
 };
 
